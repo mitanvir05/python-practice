cb986df1f5c6b0575ee3958ead40c7e9a33cdc57
Md. Muhaimin Islam Tanvir <tanvir.cse05@gmail.com>
2025-08-12 20:25:44 +0600

    Create 5.py

---
+++ 5.py
@@ -0,0 +1,8 @@
+# Write a program that takes hours and minutes as input, and calculates the total number of minutes.
+
+hours = int(input("Enter hours: "))
+minutes = int(input("Enter minutes: "))
+
+total_minutes = hours * 60 + minutes
+
+print(f"Total minutes: {total_minutes}")
