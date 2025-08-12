7d4b8c08f3beb77c70fe5109ea02e415349f537b
Md. Muhaimin Islam Tanvir <tanvir.cse05@gmail.com>
2025-08-12 20:19:55 +0600

    Create 3.py

---
+++ 3.py
@@ -0,0 +1,8 @@
+# Write a program that prints the perimeter of a rectangle to take its height and width as input.
+
+height = float(input("Enter the height of the rectangle: "))
+width = float(input("Enter the width of the rectangle: "))
+
+perimeter = 2 * (height + width)
+
+print(f"Perimeter of the rectangle: {perimeter}")
