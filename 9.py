90023b614ddc2367a398c95bfd5ffedb91c6b486
Md. Muhaimin Islam Tanvir <tanvir.cse05@gmail.com>
2025-08-12 20:35:24 +0600

    Create 9.py

---
+++ 9.py
@@ -0,0 +1,8 @@
+# Write a program to read the age of a candidate and determine whether s/he is eligible for casting vote.
+
+age = int(input("Enter your age: "))
+
+if age >= 18:
+    print("You are eligible to vote.")
+else:
+    print("You are not eligible to vote.")
