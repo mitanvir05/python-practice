b36bc9075cc11561336ce586f15a7ef07a8d5a76
Md. Muhaimin Islam Tanvir <tanvir.cse05@gmail.com>
2025-08-14 13:32:10 +0600

    Create 27.py

---
+++ 27.py
@@ -0,0 +1,11 @@
+# Write a program to print the reverse of a number.
+
+num = int(input("Enter a number: "))
+
+reverse_num = 0
+while num > 0:
+    digit = num % 10
+    reverse_num = reverse_num * 10 + digit
+    num //= 10
+
+print(f"Reversed number: {reverse_num}")
